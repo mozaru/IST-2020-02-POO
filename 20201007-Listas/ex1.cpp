e458a53805721bcbfa2ce15a4df796b0b6e3cb0c
Mozaru <mozar.silva@gmail.com>
2020-10-07 10:57:40 -0300

    lista de reais por vetor

--- 20201007-Listas/ex1.cpp
+++ 20201007-Listas/ex1.cpp
@@ -1,32 +1,3 @@
-# Lista por vetor
-
-Lista é um TAD - é uma generalização da pilha e fila
-  onde temos as seguintes as seguintes operacoes
-
-
-* __vazia__ - informa se nao existem elementos na lista
-* __cheia__ - informa se a lista esta na sua capacidade maxima
-* __obterQtd__ - retorna o numero de elementos na lista
-* __obterElemento(pos)__ - retorna o elemento da posicao pos na lista
-         normalmente o primeiro esta na posicao pos
-* __alterarElemento(pos)__ - permite alterar o valor do elemento na posicao pos da lista
-* __obterIndice(info)__ - retorna o indice do elemento info na lista
-* __existe(info)__ - retorna se a informacao info esta presente na lista
-* __inserir__ - inserir um elemento na lista
-    * __inserirNoInicio__ -> insere no inicio
-    * __inserirNoFinal__ -> insere no final
-    * __inserirOrdenado__ -> insere mantendo a ordenação da lista
-    * __inserir(pos)__ -> insere na posicao pos
-* __remover__ - remover um elemento da lista
-    * __removerNoInicio__ -> remove o primeiro
-    * __removerNoFinal__  -> remove o ultimo
-    * __remover(info)__   -> remove o elemento com a informacao info
-    * __remover(pos)__    -> remove o elemento na posicao pos da lista
-
-
-implementar uma lista de reais em cpp
-
-~~~CPP
 #include<iostream>
 #include<iomanip>
 
@@ -280,8 +251,3 @@ int main(void)
    
    return 0;	 
 }
-
-~~~
-
-
-
